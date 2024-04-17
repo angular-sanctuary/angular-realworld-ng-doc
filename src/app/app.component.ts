85faca937808b9c79d6af157e1d23d69aca2a87f
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-04-17 14:26:57 +0200

    feat: add ng-doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+import {
+  NgDocRootComponent,
+  NgDocNavbarComponent,
+  NgDocSidebarComponent,
+} from "@ng-doc/app";
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./core/layout/header.component";
 import { RouterOutlet } from "@angular/router";
@@ -7,6 +12,13 @@ import { FooterComponent } from "./core/layout/footer.component";
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    FooterComponent,
+    NgDocRootComponent,
+    NgDocNavbarComponent,
+    NgDocSidebarComponent,
+  ],
 })
 export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
+import {
+  provideNgDocApp,
+  provideSearchEngine,
+  NgDocDefaultSearchEngine,
+  providePageSkeleton,
+  NG_DOC_DEFAULT_PAGE_SKELETON,
+  provideMainPageProcessor,
+  NG_DOC_DEFAULT_PAGE_PROCESSORS,
+} from "@ng-doc/app";
+import { NG_DOC_ROUTING, provideNgDocContext } from "@ng-doc/generated";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { APP_INITIALIZER, ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withInMemoryScrolling } from "@angular/router";
 
 import { routes } from "./app.routes";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import {
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+  withFetch,
+} from "@angular/common/http";
 import { JwtService } from "./core/auth/services/jwt.service";
 import { UserService } from "./core/auth/services/user.service";
 import { apiInterceptor } from "./core/interceptors/api.interceptor";
@@ -26,5 +42,20 @@ export const appConfig: ApplicationConfig = {
       deps: [JwtService, UserService],
       multi: true,
     },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(
+      NG_DOC_ROUTING,
+      withInMemoryScrolling({
+        scrollPositionRestoration: "enabled",
+        anchorScrolling: "enabled",
+      }),
+    ),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    provideNgDocContext(),
+    provideNgDocApp(),
+    provideSearchEngine(NgDocDefaultSearchEngine),
+    providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON),
+    provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS),
   ],
 };
